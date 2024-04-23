d06d7f2365a2589fd540ebde24dc4077ad27c5d0
Savianok Marta <magnolia.sav@gmail.com>
2024-04-23 17:57:02 +0300

    end

--- assets/js/script.js
+++ assets/js/script.js
@@ -111,38 +111,160 @@ backBtns.forEach(el=> {
     })
 })
 
-
-
-const thumbsGallery = document.querySelector('.gallery-mini');
-
-if (thumbsGallery) {
-    let swiper4 = new Swiper(".gallery-mini", {
-        spaceBetween: 3,
-        slidesPerView: 5,
-        freeMode: true,
-        direction: "horizontal",
-        navigation: {
-            nextEl: ".mini-button-next",
-            prevEl: ".mini-button-prev",
-        },
-    });
-    let swiper5 = new Swiper(".gallery", {
-        spaceBetween: 10,
-        thumbs: {
-            swiper: swiper4,
-        },
-    });
-}
-
 const newProductsSwiper = document.querySelector('.new-products-swiper');
 
 if (newProductsSwiper) {
     const swiper = new Swiper('.new-products-swiper', {
-        spaceBetween: 3,
-        slidesPerView: 5,
+        spaceBetween: 22.5,
+        slidesPerView: 1,
         freeMode: true,
+        breakpoints: {
+            562: {
+                slidesPerView: 2,
+            },
+            700: {
+                slidesPerView: 2.5,
+            },
+            850: {
+                slidesPerView: 3,
+                spaceBetween: 20
+            },
+            1150: {
+                slidesPerView: 4,
+            },
+            1368: {
+                slidesPerView: 5,
+            }
+        }
 
     });
 }
+
+
+const thumbsGallerys = document.querySelectorAll('.gallery-mini');
+
+if (thumbsGallerys) {
+    const swipers = {};
+
+    thumbsGallerys.forEach((el , i) => {
+        const swiperName = `swiper${i}`;
+
+        const currentBtnNext = el.closest('.product-card').querySelector(".mini-button-next");
+        const currentBtnPrev = el.closest('.product-card').querySelector(".mini-button-prev");
+
+        swipers[swiperName] = new Swiper(el, {
+            spaceBetween: 3,
+            slidesPerView: 5,
+            freeMode: true,
+            direction: "horizontal",
+            navigation: {
+                nextEl: currentBtnNext,
+                prevEl: currentBtnPrev,
+            },
+        });
+
+        const currGallery = el.closest('.product-card').querySelector(".gallery");
+        const thumbSwiperName = `thumbSwiper${i}`;
+        swipers[thumbSwiperName] = new Swiper(currGallery, {
+            spaceBetween: 10,
+            thumbs: {
+                swiper: swipers[swiperName],
+            },
+        });
+    });
+}
+
+
+
+// collection-filter
+
+const filterItems = document.querySelectorAll('.collection .filter li');
+
+function clearClasses() {
+    filterItems.forEach(el => {
+        if (el.classList.contains('active')) {
+            el.classList.remove('active')
+        }
+    })
+}
+filterItems?.forEach(el=> {
+    el.addEventListener('click' ,  ()=> {
+        clearClasses();
+        el.classList.add('active')
+    })
+})
+
+// page-navigation
+
+const pageNavigation = document.querySelectorAll('.page-nav ul li');
+
+function clearPageNavClasses() {
+    pageNavigation.forEach(el => {
+        if (el.classList.contains('active')) {
+            el.classList.remove('active')
+        }
+    })
+}
+
+pageNavigation?.forEach(el=> {
+    el.addEventListener('click' ,  ()=> {
+        clearPageNavClasses();
+        el.classList.add('active')
+    })
+})
+
+
+// single-page
+
+const modelsList = document.querySelectorAll('.model-list li')
+function clearModelsClasses() {
+    modelsList.forEach(el => {
+        if (el.classList.contains('active')) {
+            el.classList.remove('active')
+        }
+    })
+}
+
+modelsList?.forEach(el=> {
+    el.addEventListener('click' ,  ()=> {
+        clearModelsClasses();
+        el.classList.add('active')
+    })
+})
+
+// tabs
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    const tabsBtns = document.querySelectorAll('.tabs-panel li');
+    const cars = document.querySelectorAll('.tab-container .tab-content');
+
+
+    if(tabsBtns){
+        tabsBtns.forEach(function(el){
+            el.addEventListener('click', function(ev){
+                makeActive(ev.target);
+            });
+        });
+    }
+    function makeActive(target){
+        let num = target.dataset.num;
+        let selector = '.tab-container .tab-content[data-num="'+num+'"]';
+        let targetTab = document.querySelector(selector);
+
+        tabsBtns.forEach(function(el){
+            el.classList.remove('active');
+        });
+        cars.forEach(function(el){
+            el.classList.remove('active');
+        });
+        target.classList.add('active');
+        targetTab.classList.add('active');
+
+    }
+
+
+});
+
+
 
 
